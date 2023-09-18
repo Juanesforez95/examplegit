088c818257a9e5925ea60fb0656ba01dc0124bbc
Juan Esteban Florez <juanesforez95@gmail.com>
2023-09-18 10:58:10 -0500

    prints de inicio
    se agregaron dos prints al inicio del archivo

---
+++ Example1.py
@@ -0,0 +1,27 @@
+"""
+Fecha:18/09/2023
+Autor:Juan Esteban
+Objetivo:Ejemplo de versionamiento con git desde python
+"""
+
+import random
+
+print("Número aleatorio entre 1 y 10")
+random_number = random.randint(1, 10)
+print(random_number)
+
+for i in  range(0, 10):
+    random_number = random.randrange(20, 100, 5)
+    print(random_number)
+
+print("----------------------------------------")
+
+for i in  range(0, 10):
+    random_number = random.random()
+    print(random_number)
+
+print("----------------------------------------")   
+
+for i in  range(0, 10):
+    random_number = random.uniform(10.5, 20.6)
+    print(random_number)
